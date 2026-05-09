232d5d15c1b92dd839d072796ca58c1ef72610b4
David Uhden Collado <david@uhden.dev>
2026-05-09 23:05:40 +0200

    Update dprint plugin

--- scripts/validate-website.pl
+++ scripts/validate-website.pl
@@ -557,7 +557,7 @@ if ( @css || @js || @json ) {
               . "  \"lineWidth\": 80,\n"
               . "  \"newLineKind\": \"lf\",\n"
               . "  \"plugins\": [\n"
-              . "    \"https://plugins.dprint.dev/typescript-0.95.15.wasm\",\n"
+              . "    \"https://plugins.dprint.dev/typescript-0.96.0.wasm\",\n"
               . "    \"https://plugins.dprint.dev/g-plane/malva-v0.15.3.wasm\",\n"
               . "    \"https://plugins.dprint.dev/json-0.21.3.wasm\"\n"
               . "  ]\n"
